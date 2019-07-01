c4efe0496d9b86f0a811b8b9e0cf324537518085
Omar Adel <omar.adell96@gmail.com>
2019-07-01 20:56:09 +0200

    example commit

--- IDEA_GitExample/IDEA_GitExample/ViewController.swift
+++ IDEA_GitExample/IDEA_GitExample/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var x = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
